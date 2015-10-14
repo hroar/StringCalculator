4a2b647ff0082dbbd29977f784fc530949c523e5
hroar <jonasj14@ru.is>
2015-10-14 22:24:37 +0000

    refactoring testemptystring

--- src/test/is/ru/Calculator/StringCalculatorTest.java
+++ src/test/is/ru/Calculator/StringCalculatorTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 
 public class StringCalculatorTest 
 {
-	private StringCalculator calculator = new StringCalculator();
+	/*private StringCalculator calculator = new StringCalculator();*/
 	public static void main(String args[])
 	{
-		org.junit.runner.JUnitCore.main("is.ru.Calculator");
+		org.junit.runner.JUnitCore.main("is.ru.Calculator.StringCalculatorTest");
 	}
 
 	@Test
 	public void testEmptyString()
 	{
-		assertEquals(0, calculator.add(""));
+		assertEquals(StringCalculator.add(""),0);
 	}
 }
